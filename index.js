0cd605d57f2049afd3fb8d41ecfc702d0df68d63
breannams <breannamarie_s@hotmail.com>
2021-07-06 23:17:02 -0400

    commit tree 436acb6aadedf33ada434e564d7c20e0e3f85b7a

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun(activity ="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(str = "*"){
+    return function(adjective = "special"){
+        return `You are ${str}${adjective}${str}!`
+    }
+}
+
+
+const Calculator = {
+    add: function(a,b){
+        return  a + b
+    },
+    subtract: function(a,b){
+        return a -b
+    },
+
+    multiply: function(a,b){
+        return  a * b
+    },
+
+    divide: function(a,b){
+        return a / b
+    }
+}
+
+
+function actionApplyer(start, arr){
+    arr.forEach(starting =>{
+        start = starting(start)
+    })
+    return start
+}
